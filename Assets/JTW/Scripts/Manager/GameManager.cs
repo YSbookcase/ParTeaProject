b32162ca516ad4027c99f3b9777686c820cb60e2
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-07-24 00:16:06 +0900

    [Feat] GameManager 기본 기능 구현
    - 처음 게임을 시작하는 GameStart, 게임이 끝나고 Room으로 돌아가는 GameEnd, 미니게임이 끝나고 점수 집계화면으로 넘어가는 GoScoreScene  함수 추가.

--- Assets/JTW/Scripts/Manager/GameManager.cs
+++ Assets/JTW/Scripts/Manager/GameManager.cs
@@ -1,8 +1,29 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
-    
+    public void GameStart(string sceneName)
+    {
+        PhotonNetwork.CurrentRoom.IsOpen = false;
+        PhotonNetwork.CurrentRoom.IsVisible = false;
+
+        PhotonNetwork.LoadLevel(sceneName);
+    }
+
+    public void GameEnd()
+    {
+        PhotonNetwork.CurrentRoom.IsOpen = true;
+        PhotonNetwork.CurrentRoom.IsVisible = true;
+
+        // TODO : Room 씬으로 돌아간다.
+        PhotonNetwork.LoadLevel("Main");
+    }
+
+    public void GoScoerScene()
+    {
+        PhotonNetwork.LoadLevel("Score");
+    }
 }
