e440b6b74525d1d203a76250cdf7b8346f82aafe
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-07-24 00:01:40 +0900

    [Feat] Photon Player 확장 함수 추가
    - SetRank : 플레이어에 커스텀 프로퍼티 rank를 추가하고, 매개변수로 전달 받은 값으로 rank 값을 지정한다.
    
    - GetRank : 플레이어의 커스텀 프로퍼티인 rank를 int 형으로 반환받는다.

---
+++ Assets/JTW/Scripts/Extension/RankExtensions.cs
@@ -0,0 +1,27 @@
+using Photon.Pun.UtilityScripts;
+using Photon.Realtime;
+using ExitGames.Client.Photon;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class RankExtensions
+{
+    public static void SetRank(this Player player, int newRank)
+    {
+        Hashtable rank = new Hashtable();  
+        rank["rank"] = newRank;
+
+        player.SetCustomProperties(rank);  
+    }
+
+    public static int GetRank(this Player player)
+    {
+        object rank;
+        if (player.CustomProperties.TryGetValue("rank", out rank))
+        {
+            return (int)rank;
+        }
+
+        return 0;
+    }
+}
