0dd76a464e8409887019df03887fd2d90e127a9e
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-07-24 17:14:55 +0900

    [Feat] ObstacleHandler 스크립트 추가
    - 장애물의 이동 및 파괴를 진행하는 로직 추가.

---
+++ Assets/JTW/Scripts/JumpGame/ObstacleHandler.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleHandler : MonoBehaviour
+{
+    private Rigidbody rigid;
+
+    private Vector3 direction = Vector3.zero;
+    private float speed;
+
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        rigid.velocity = direction * speed;
+    }
+
+    public void Init(Vector3 direction, float speed)
+    {
+        this.direction = direction;
+        this.speed = speed;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Finish"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
