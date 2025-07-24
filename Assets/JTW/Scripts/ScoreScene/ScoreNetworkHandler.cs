c1ea1c1765a956be12c4e929780fbd00055410ac
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-07-24 15:30:15 +0900

    [Feat] ScoreNetworkHandler 스크립트 추가
    - Score 씬에서 Photon과 관련될 처리를 할 ScoreNetworkHandler 스크립트 추가.
    - 플레이어의 순위 정보가 모두 갱신이 완료되었다면, 스코어 보드의 초기화를 진행한다.

---
+++ Assets/JTW/Scripts/ScoreScene/ScoreNetworkHandler.cs
@@ -0,0 +1,50 @@
+using JTW_JumpGame;
+using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreNetworkHandler : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private ScoreUIPresenter scorePresenter;
+
+    private void Start()
+    {
+        if (isAllRankUpdated())
+        {
+            scorePresenter.InitScore();
+        }
+    }
+
+    public override void OnPlayerPropertiesUpdate(Player targetPlayer, ExitGames.Client.Photon.Hashtable changedProps)
+    {
+        if (changedProps.ContainsKey("rank"))
+        {
+            if (isAllRankUpdated())
+            {
+                scorePresenter.InitScore();
+            }
+        }
+    }
+
+    private bool isAllRankUpdated()
+    {
+        foreach (Player player in PhotonNetwork.PlayerList)
+        {
+            if (player.CustomProperties.ContainsKey("rank"))
+            {
+                if ((int)player.CustomProperties["rank"] == 0)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
