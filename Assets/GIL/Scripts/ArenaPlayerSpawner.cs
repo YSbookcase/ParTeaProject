81e707a00758cdfafe51b414877f47ff01f333b1
Hangil Chang <hangil2010@naver.com>
2025-07-24 16:46:18 +0900

    [Feat] ArenaSpawner 생성 완료
    - 2인 이상 연결되었을 경우 플레이어를 소환하게 함
    - 우선 자체적으로 로비,룸 생성 및 참가 테스트 코드 생성
       - 포톤 네트워크에 연결될 경우 랜덤 방 진입 or 방 생성을 진행
       - 2명 이상 방에 진입 했을 때 게임 실행, 360도를 플레이어 수로 나누어 원에 소환하는 방식으로 진행
         - 2명일 경우 0 , 180 3명일 경우 0, 120 ,240도에 소환되게 함
    - Todo : 타이틀->게임 진입 부분이 완성 될 경우 ArenaPlayerSpawner로 이동할 필요 있음

---
+++ Assets/GIL/Scripts/ArenaPlayerSpawner.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class ArenaPlayerSpawner : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private GameObject playerPrefab;
+    [SerializeField] private float spawnRadius = 5f;
+    [SerializeField] private Vector3 center = Vector3.zero;
+
+    private int _playerCount;
+    
+    private void Start()
+    {
+        SpawnPlayer();
+    }
+
+    private void SpawnPlayer()
+    {
+        _playerCount = PhotonNetwork.CurrentRoom.PlayerCount;
+        int myIndex = GetPlayerIndex();
+
+        // 각도를 등분함
+        float anglePerPlayer = 360f / _playerCount;
+        float angle = anglePerPlayer * myIndex;
+        float rad = angle * Mathf.Deg2Rad;
+
+        // 위치 계산 (Y는 0 또는 원하는 높이)
+        Vector3 spawnPos = new Vector3(
+            center.x + Mathf.Cos(rad) * spawnRadius,
+            center.y,
+            center.z + Mathf.Sin(rad) * spawnRadius
+        );
+
+        // 생성
+        PhotonNetwork.Instantiate(playerPrefab.name, spawnPos, Quaternion.identity);
+    }
+
+    private int GetPlayerIndex()
+    {
+        var players = PhotonNetwork.CurrentRoom.Players;
+        int index = 0;
+        foreach (var kvp in players)
+        {
+            if (kvp.Value == PhotonNetwork.LocalPlayer)
+                return index;
+            index++;
+        }
+        return 0; // fallback
+    }
+}
