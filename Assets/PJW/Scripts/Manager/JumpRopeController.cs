1ab24fba2db42bd579ba06b72ad3ecac533be1b6
jongwon1205 <haegol5009@gmail.com>
2025-07-24 14:26:56 +0900

    [Feat] RopeController
    로프의 자동 회전 기능과
    로프에 닿으면 날아가는 방식 추가

---
+++ Assets/PJW/Scripts/Manager/JumpRopeController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RopeGame
+{
+    public class JumpRopeController : MonoBehaviour
+    {
+        [Header("회전 대상")]
+        public Transform ropeTransform;
+
+        [Header("회전 중심")]
+        public Transform centerPoint;
+
+        [Header("회전 설정")]
+        public Vector3 rotationAxis = Vector3.up;
+        public float initialSpeed = 30f;
+        public float maxSpeed = 360f;
+        public float acceleration = 10f;
+
+        private float currentSpeed;
+
+        void Start()
+        {
+            currentSpeed = initialSpeed;
+        }
+
+        void Update()
+        {
+            currentSpeed = Mathf.Min(currentSpeed + acceleration * Time.deltaTime, maxSpeed);
+
+            // centerPoint기준으로 회전
+            ropeTransform.RotateAround(centerPoint.position, rotationAxis, currentSpeed * Time.deltaTime);
+        }
+    }
+}
+
---
+++ Assets/PJW/Scripts/Player/PlayerController.cs
@@ -0,0 +1,71 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RopeGame
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private float jumpForce = 5f;
+        [SerializeField] private float bounceForce = 25f;
+        [SerializeField] private GameObject ropeObject;
+
+        private Rigidbody playerRigidbody;
+        private bool isGrounded;
+        private bool isDead = false;
+
+        private void Awake()
+        {
+            playerRigidbody = GetComponent<Rigidbody>();
+            playerRigidbody.sleepThreshold = 0f; // 가만히 있는 상태에서도 리지드 바디를 적용시킴
+        }
+
+        private void Update()
+        {
+            if (isDead) return;
+
+            if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+            {
+                Jump();
+            }
+        }
+
+        private void Jump()
+        {
+            Vector3 velocity = playerRigidbody.velocity;
+            velocity.y = jumpForce;
+            playerRigidbody.velocity = velocity;
+            isGrounded = false; 
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.CompareTag("Ground"))
+            {
+                isGrounded = true;
+            }
+            else if (!isDead && collision.gameObject == ropeObject)
+            {
+                Die();
+            }
+        }
+
+        private void OnCollisionExit(Collision collision)
+        {
+            if (collision.gameObject.CompareTag("Ground"))
+            {
+                isGrounded = false;
+            }
+        }
+
+        private void Die()
+        {
+            if (isDead) return;
+            isDead = true;
+
+            Vector3 bounceDir = (Vector3.up + Random.onUnitSphere).normalized;
+            playerRigidbody.AddForce(bounceDir * bounceForce, ForceMode.Impulse); // 로프에 닿으면 날아감
+        }
+    }
+}
