39aa71f2f1333b425337b245668174e9fe3bd78d
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-07-24 12:56:35 +0900

    [Feat] JumpGameHandler 스크립트 추가
    - 플레이어가 모두 로딩이 완료되기를 기다리고 게임을 시작하는 로직 구현.
    - Test용으로 Game을 완료하는 버튼 추가.
    - Game 완료 시, 각 Player의 Rank를 지정하고 Score 씬으로 이동.
    - JumpGame 씬에 해당 스크립트를 추가한 오브젝트 추가.

---
+++ Assets/JTW/Scripts/JumpGame/JumpGameHandler.cs
@@ -0,0 +1,57 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace JTW_JumpGame
+{
+    public class JumpGameHandler : MonoBehaviourPunCallbacks
+    {
+        [Header("테스트 용")]
+        [SerializeField] private Button gameEndButton;
+
+        private bool isGameStarted;
+
+        private void Awake()
+        {
+            gameEndButton.onClick.AddListener(GameEnd);
+        }
+
+        private void GameStart()
+        {
+            if (isGameStarted) return;
+            isGameStarted = true;
+
+            Debug.Log("점프 게임 시작!");
+            // TODO : 점프 게임 로직 구현
+        }
+
+        private void GameEnd()
+        {
+            // TODO : 점수에 따른 순위 확정 로직 작성
+
+            int rank = 1;
+            foreach(Player player in PhotonNetwork.PlayerList)
+            {
+                player.SetRank(rank);
+                rank++;
+            }
+
+            Manager.game.GoScoerScene();
+        }
+
+        public override void OnPlayerPropertiesUpdate(Player targetPlayer, ExitGames.Client.Photon.Hashtable changedProps)
+        {
+            if (changedProps.ContainsKey("isLoaded"))
+            {
+                if (Manager.game.isAllPlayerLoaded())
+                {
+                    GameStart();
+                }
+            }
+        }
+    }
+}
+
