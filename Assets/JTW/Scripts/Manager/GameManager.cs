1a46e4590c069c8ea01a5ba3ce43c2dadf842cb2
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-07-24 12:29:20 +0900

    [Feat] GameManager에 미니게임 씬으로 넘어가는 기능 구현
    - string 값을 받아서 해당하는 씬으로 이동하는 기능 구현.
    - 미니게임 씬으로 이동 시, Player 각자의 isLoaded 프로퍼티를 false로 변경.
    - 씬의 로딩이 완료되면 각자 isLoaded 프로퍼티가 true가 되도록 기능 구현.
    - 각 미니게임 씬에서 OnPlayerPropertiesUpdate 함수를 통해, 모든 플레이어의 로딩이 완료될 경우 게임을 시작하도록 하려는 의도로 제작함.

--- Assets/JTW/Scripts/Manager/GameManager.cs
+++ Assets/JTW/Scripts/Manager/GameManager.cs
@@ -3,9 +3,22 @@ using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
 
 public class GameManager : Singleton<GameManager>
 {
+    private void OnEnable()
+    {
+        // 각 플레이어들이 로컬에서 이벤트 등록을 할 수 있도록 OnEnable에서 진행.
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
     public void GameStart(string sceneName)
     {
         PhotonNetwork.CurrentRoom.IsOpen = false;
@@ -17,7 +30,7 @@ public class GameManager : Singleton<GameManager>
             player.SetTotalGameScore(0);
         }
 
-        PhotonNetwork.LoadLevel(sceneName);
+        GoNextMiniGame(sceneName);
     }
 
     public void GameEnd()
@@ -32,5 +45,30 @@ public class GameManager : Singleton<GameManager>
     public void GoScoerScene()
     {
         PhotonNetwork.LoadLevel("Score");
+    }
+
+    public void GoNextMiniGame(string sceneName)
+    {
+        // 플레이어들이 미니게임 씬으로 넘어갈 때,
+        // 모두가 로딩이 완료되었는지 확인하기 위해 isLoaded를 사용한다.
+        foreach(Player player in PhotonNetwork.PlayerList)
+        {
+            Hashtable isLoaded = new Hashtable();
+            isLoaded["isLoaded"] = false;
+
+            player.SetCustomProperties(isLoaded);
+        }
+
+        PhotonNetwork.LoadLevel(sceneName);
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
+    {
+        if (PhotonNetwork.LocalPlayer == null) return;
+
+        Hashtable isLoaded = new Hashtable();
+        isLoaded["isLoaded"] = true;
+
+        PhotonNetwork.LocalPlayer.SetCustomProperties(isLoaded);
     }
 }
