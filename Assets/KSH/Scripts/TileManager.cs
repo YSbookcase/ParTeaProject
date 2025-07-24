4b9710cbadf2560ccede2243e627cbf5c60d0c1c
강소현 <conut1119@gmail.com>
2025-07-24 17:40:07 +0900

    [Feat] TileManager 생성
    타일 갯수를 세주는 기능 구현

--- Assets/KSH/Scripts/TileManager.cs
+++ Assets/KSH/Scripts/TileManager.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TileManager : MonoBehaviour
+namespace KSH
 {
-    // Start is called before the first frame update
-    void Start()
+    public class TileManager : MonoBehaviour
     {
+        public int redTileCount;
+        public int blueTileCount;
         
-    }
+        public static TileManager Instance;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        private void Awake()
+        {
+            if(Instance == null) // Instance가 null이면
+            {
+                Instance = this; // 할당
+            }
+            else // 이미 존재한다면
+            {
+                Destroy(gameObject); // 하나만 존재해야 하므로 제거
+            }
+        }
+
+        public void CountTile(Color color)
+        {
+            if(color == Color.red)
+                redTileCount++;
+            else if(color == Color.blue)
+                blueTileCount++;
+        }
+    }    
 }
