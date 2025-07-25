9effacccb2e068600bbbc0653cece8eb62664e10
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-07-25 16:59:14 +0900

    [Feat] 점프게임 진행 로직 구현
    - 게임 시작 이후 장애물 생성 코루틴 실행.
    - 장애물이 하나 지나간 후에, 살아남은 플레이어에게 1점 부여.
    - 게임 종료 시, 점수가 높은 순으로 순위 부여 후 점수 집계 씬으로 이동.

--- Assets/JTW/Scripts/JumpGame/JumpGameHandler.cs
+++ Assets/JTW/Scripts/JumpGame/JumpGameHandler.cs
@@ -2,6 +2,7 @@
 using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,10 +11,16 @@ namespace JTW_JumpGame
     public class JumpGameHandler : MonoBehaviourPunCallbacks
     {
         [SerializeField] private GameObject playerPrefab;
-        [SerializeField] private Vector3 obstacleSpawnPoint;
+        [SerializeField] private GameObject obstaclePrefab;
+
+        [SerializeField] private Transform obstacleSpawnPoint;
 
         [Header("테스트 용")]
         [SerializeField] private Button gameEndButton;
+
+        private GameObject localPlayer;
+
+        private List<int> alivePlayers = new List<int>();
 
         private Vector3 playerSpawnPoint = new Vector3(-2.25f, 0, 0);
         private bool isGameStarted;
@@ -25,28 +32,101 @@ namespace JTW_JumpGame
 
 
         [PunRPC]
-        private void JumpGameStart()
+        private void JumpGameStart(PhotonMessageInfo info)
         { 
             if (isGameStarted) return;
             isGameStarted = true;
 
             playerSpawnPoint.x += 1.5f * (PhotonNetwork.LocalPlayer.ActorNumber - 1);
 
-            PhotonNetwork.Instantiate("JTW_JumpPlayer", playerSpawnPoint, Quaternion.identity);
+            localPlayer = PhotonNetwork.Instantiate("JTW_JumpPlayer", playerSpawnPoint, Quaternion.identity);
 
             Debug.Log("점프 게임 시작!");
-            // TODO : 점프 게임 로직 구현
+
+            float lag = Mathf.Abs((float)(PhotonNetwork.Time - info.SentServerTime));
+
+            StartCoroutine(JumpGameCoroutine(lag));
+        }
+
+        private IEnumerator JumpGameCoroutine(float lag)
+        {
+            // 지연 보상
+            float startDelay = 2f - lag;
+
+            yield return new WaitForSeconds(startDelay);
+
+            float obstacleSpeed = 3f;
+
+            float timer = 0;
+
+            while (timer <= 60)
+            {
+                GameObject obstacle = Instantiate(obstaclePrefab, obstacleSpawnPoint.position, Quaternion.Euler(new Vector3(90, 0, 0)));
+                obstacle.GetComponent<ObstacleHandler>().Init(Vector3.right, obstacleSpeed);
+                obstacleSpeed += 0.5f;
+
+                while (true)
+                {
+                    if (obstacle == null) break;
+
+                    timer += Time.deltaTime;
+                    yield return null;
+                }
+
+                if(localPlayer != null)
+                {
+                    PhotonNetwork.LocalPlayer.AddJumpGameScore(1);
+                }
+                else
+                {
+                    photonView.RPC("DeleteJumpGamePlayer", RpcTarget.MasterClient);
+                }
+
+                timer += 2;
+                yield return new WaitForSeconds(2f);
+
+                if(PhotonNetwork.IsMasterClient && alivePlayers.Count <= 0)
+                {
+                    break;
+                }
+            }
+
+            if (PhotonNetwork.IsMasterClient)
+            {
+                GameEnd();
+            }
+
+        }
+
+        [PunRPC]
+        private void DeleteJumpGamePlayer(PhotonMessageInfo info)
+        {
+            if (!PhotonNetwork.IsMasterClient) return;
+
+            alivePlayers.Remove(info.Sender.ActorNumber);
         }
 
         private void GameEnd()
         {
-            // TODO : 점수에 따른 순위 확정 로직 작성
+            List<Player> players = PhotonNetwork.PlayerList.OrderByDescending(p => p.GetJumpGameScore()).ToList();
 
-            int rank = 1;
-            foreach(Player player in PhotonNetwork.PlayerList)
+            int rank = -1;
+            int score = -1;
+
+            for(int i = 0; i < players.Count; i++)
             {
-                player.SetRank(rank);
-                rank++;
+                // 이전 플레이어와 같은 점수일 경우, 같은 등수를 부여한다.
+                // 예) 1등(3점), 1등(3점), 1등(3점), 4등(2점)
+                if(score == players[i].GetJumpGameScore())
+                {
+                    players[i].SetRank(rank);
+                }
+                else
+                {
+                    players[i].SetRank(i + 1);
+                    rank = i + 1;
+                    score = players[i].GetJumpGameScore();
+                }
             }
 
             Manager.game.GoScoerScene();
@@ -60,6 +140,12 @@ namespace JTW_JumpGame
             {
                 if (Manager.game.isAllPlayerLoaded())
                 {
+                    foreach(Player player in PhotonNetwork.PlayerList)
+                    {
+                        alivePlayers.Add(player.ActorNumber);
+                        player.SetJumpGameScore(0);
+                    }
+
                     photonView.RPC("JumpGameStart", RpcTarget.AllViaServer);
                 }
             }
