d73d25e5a0faf7d0e9a85e55db51b7f7c63c72b9
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-07-28 15:50:19 +0900

    [Feat] Manager에 네트워크 관련 Manager들 추가
    - Photon, Firebase관련 Manager 추가.
    - 네트워크 씬 관련 UI Manager 추가.

--- Assets/JTW/Scripts/Manager/Manager.cs
+++ Assets/JTW/Scripts/Manager/Manager.cs
@@ -1,3 +1,4 @@
+using KYS;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,10 +6,16 @@ using UnityEngine;
 public static class Manager
 {
     public static GameManager game => GameManager.GetInstance();
+    public static KYS.UIManager UI => KYS.UIManager.GetInstance();
+    public static PhotonManager Photon => PhotonManager.GetInstance();
+    public static FirebaseManager Firebase => FirebaseManager.GetInstance();
 
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
     private static void Initailize()
     {
         GameManager.CreateInstance();
+        KYS.UIManager.CreateInstance();
+        PhotonManager.CreateInstance();
+        FirebaseManager.CreateInstance();
     }
 }
