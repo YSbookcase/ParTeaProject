ac9be6e1b13fa432527f2757421f879d93dd151f
Lazism2j <lazism2j@gmail.com>
2025-07-28 23:35:58 +0900

    [Feat] RacingController Goal 도착 구현

--- Assets/LJ2/Scripts/RacingLine.cs
+++ Assets/LJ2/Scripts/RacingLine.cs
@@ -7,5 +7,31 @@ public class RacingLine : MonoBehaviour
     [SerializeField] public List<Transform> spawnPositions = new List<Transform>();
     
     [SerializeField] public float pathPosition;
+    [SerializeField] public GameObject bottomLine;
+    [SerializeField] public GameObject goalQuad;
 
+    public int havePassLine;
+
+    private void Awake()
+    {
+        bottomLine.SetActive(false);
+        goalQuad.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        RacingController controller = other.GetComponent<RacingController>();
+        if (controller != null) 
+        {
+            if (controller.linePassed < havePassLine) 
+            {
+                controller.linePassed++;
+            }
+            else
+            {
+                Debug.Log("플레이어 도착");
+            }
+        }
+        
+    }
 }
