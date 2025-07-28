edff76e7cbb33008bba85bae62c49b2866f99ef7
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-07-28 15:26:32 +0900

    [Feat] SingtonPunCallback 스크립트 추가
    - MonoBehaviourPunCallback을 상속받은 Sington 객체를 만들기 위해 SingtonPunCallback 스크립트 추가.

---
+++ Assets/JTW/Scripts/DesignPattern/SingtonPunCallback.cs
@@ -0,0 +1,32 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingtonPunCallback<T> : MonoBehaviourPunCallbacks where T : MonoBehaviourPunCallbacks
+{
+    private static T instance;
+    public static void CreateInstance()
+    {
+        if (instance == null)
+        {
+            T prefab = Resources.Load<T>(typeof(T).Name);
+            instance = Instantiate(prefab);
+            DontDestroyOnLoad(instance.gameObject);
+        }
+    }
+
+    public static void ReleaseInstance()
+    {
+        if (instance != null)
+        {
+            Destroy(instance.gameObject);
+            instance = null;
+        }
+    }
+
+    public static T GetInstance()
+    {
+        return instance;
+    }
+}
