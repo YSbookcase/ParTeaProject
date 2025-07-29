cf82f75a8a5992a08eacacf00eff5d529524a726
jongwon1205 <haegol5009@gmail.com>
2025-07-29 15:45:58 +0900

    [Refactor] GameStart
    기존에서는 보간으로 진행하였지만  게임 시작시 같이 시작을 시작하는 것으로 변경

--- Assets/PJW/Scripts/Manager/RopeGameManager.cs
+++ Assets/PJW/Scripts/Manager/RopeGameManager.cs
@@ -1,15 +1,18 @@
 using PJW;
 using Photon.Pun;
 using Photon.Realtime;
+using ExitGames.Client.Photon;
 using System.Collections;
+using System.Linq;
 using UnityEngine;
 using TMPro;
+using PhotonHashtable = ExitGames.Client.Photon.Hashtable;
 
 namespace PJW
 {
     public class RopeGameManager : MonoBehaviourPunCallbacks
     {
-        [Header("UI")]
+        [Header("UI")]  
         [SerializeField] private TextMeshProUGUI countdownText;
 
         [Header("Rank 계산기")]
@@ -18,16 +21,43 @@ namespace PJW
         private int totalPlayers;
         private int deathCount = 0;
 
+        private const string IsLoadedKey = "isRopeLoaded";
+
         private void Start()
         {
             totalPlayers = PhotonNetwork.CurrentRoom.PlayerCount;
+
+            var props = new PhotonHashtable { { IsLoadedKey, true }};
+            PhotonNetwork.LocalPlayer.SetCustomProperties(props);
+        }
+
+        public override void OnPlayerPropertiesUpdate(Player targetPlayer, PhotonHashtable changedProps)
+        {
+            if (!PhotonNetwork.IsMasterClient) return;
+
+            if (changedProps.ContainsKey(IsLoadedKey))
+            {
+                bool allLoaded = PhotonNetwork.PlayerList
+                    .All(p => p.CustomProperties.ContainsKey(IsLoadedKey)
+                           && (bool)p.CustomProperties[IsLoadedKey]);
+
+                if (allLoaded)
+                {
+                    photonView.RPC("RPCRopeBeginCountdown", RpcTarget.AllViaServer);
+                }
+            }
+        }
+
+        // RPC로 호출되는 카운트다운 시작
+        [PunRPC]
+        private void RPCRopeBeginCountdown()
+        {
             BeginCountdown();
         }
 
         public void BeginCountdown()
         {
             deathCount = 0;
-
             StopAllCoroutines();
             StartCoroutine(CountdownRoutine());
         }
@@ -71,7 +101,6 @@ namespace PJW
                 return;
 
             rankCalculator?.CalculateRanks();
-
             PhotonNetwork.LoadLevel("Score");
         }
     }
