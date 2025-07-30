d8412f99cbfada2010320b69c97d29a61cf9193e
강소현 <conut1119@gmail.com>
2025-07-30 17:09:26 +0900

    [Chore] 장애물 밀치는 힘 수정, 스폰 위치 수정

--- Assets/KSH/Scripts/Managers/TestNetwork.cs
+++ Assets/KSH/Scripts/Managers/TestNetwork.cs
@@ -39,7 +39,7 @@ namespace KSH
 
         private void PlayerSpawn()
         {
-            Vector3 spawnPos = new Vector3(Random.Range(0, 48), 2, Random.Range(0, 14));
+            Vector3 spawnPos = new Vector3(Random.Range(5, 48), 2, Random.Range(0, 14));
             GameObject player = PhotonNetwork.Instantiate("Player_KSH", spawnPos, Quaternion.identity);
         }
     }    
