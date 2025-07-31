e24ce4b829cb2ec8a9c17b0df3d498f0e2a2d33f
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-07-31 15:19:54 +0900

    [Feat] AudioData 구조 변경
    - Address 패키지를 사용 안함에 따라 그냥 AudioClip으로 clip 변경.
    - loop는 AudioManager에서 관리하므로, 삭제.

--- Assets/JTW/Scripts/Manager/Audio/AudioData.cs
+++ Assets/JTW/Scripts/Manager/Audio/AudioData.cs
@@ -1,15 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AddressableAssets;
 
 [System.Serializable]
 [CreateAssetMenu(fileName = "AudioData", menuName = "Audio/Data")]
 public class AudioData : ScriptableObject
 {
     public string clipName;
-    public AssetReferenceT<AudioClip> clipRef;
+    public AudioClip clip;
     [Range(0f, 1f)]
     public float volume = 1.0f;
-    public bool loop = false;
 }
