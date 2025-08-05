25df81de751ab9bee489f8ef0d6b739d308bb9ab
강소현 <conut1119@gmail.com>
2025-08-06 01:41:48 +0900

    [Chore] 승리 UI 대사 수정

--- Assets/KSH/Scripts/Managers/UIManager.cs
+++ Assets/KSH/Scripts/Managers/UIManager.cs
@@ -104,11 +104,11 @@ namespace KSH
             int blueTile = TileManager.Instance.blueTileCount;
 
             if (redTile > blueTile)
-                WinnerUIUpdate("Winner Team : RedTeam!", Color.red);
+                WinnerUIUpdate("레드팀 우승!", Color.red);
             else if(blueTile > redTile)
-                WinnerUIUpdate("Winner Team : BlueTeam!", Color.blue);
+                WinnerUIUpdate("블루팀 우승!", Color.blue);
             else
-                WinnerUIUpdate("Draw", Color.green);
+                WinnerUIUpdate("무승부!", Color.green);
             
         }
 
